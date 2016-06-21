fac1126b575d7228d4364da6d400f48f7d4c7a8b
Katie Vogel <katiebvogel@gmail.com>
2016-06-21 15:39:59 -0500

    added on to Andrew's work

--- app.js
+++ app.js
@@ -22,3 +22,22 @@ console.log(provided.charAt(1));
 console.log(provided.toUpperCase());
 
 console.log(provided.replace('!','?'));
+
+console.log(provided.replace('H', "'").substring(0,5));
+
+
+
+var splitProvided = provided.split('');
+
+var provArray = Array(splitProvided);
+console.log(splitProvided.reverse().join(''));
+//this reversed the order of the elements in our array, but kept the commas
+
+
+
+
+var elevenEleven = (provided.length - 1)* 101;
+
+console.log(elevenEleven.toString());
+
+console.log(provided.split(' '));
