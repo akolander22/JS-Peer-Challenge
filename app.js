d5407ed73a7a80d28f58b9bc5d4c38a6537d5f5b
Andrew Kolander <andrewkolander@MacBook-Pro-3.local>
2016-06-21 14:35:47 -0500

    commit

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 var provided = 'Hello World!';
+
+console.log(Number(provided === 'Hello World!'));
+// this returns a number of 1
+
+var provided = 'Hello World!';
+
+console.log(Number(provided));
+// returns NaN
+
+var provided = 'Hello World!';
+
+var myHello = provided.substring(0, 5);
+console.log(myHello.split(''));
+
+console.log(typeof provided);
+
+console.log((provided.length)-3);
+
+console.log(provided.charAt(1));
+
+console.log(provided.toUpperCase());
+
+console.log(provided.replace('!','?'));
