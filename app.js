7e85d0e46488ba94c35ef1a029f76168214bb3af
Andrew Kolander <andrewkolander@MacBook-Pro-3.local>
2016-06-21 15:54:01 -0500

    actually made fix

--- app.js
+++ app.js
@@ -23,18 +23,7 @@ console.log(provided.toUpperCase());
 
 console.log(provided.replace('!','?'));
 
-<<<<<<< HEAD
-var splitWord = provided.split('');
-var answer = splitWord.reverse();
-console.log(answer.join(''));
-//couldn't get rid of the commas in the reverse
 
-console.log(provided.split(' '));
-
-var laterPart = (provided.length-1)*101;
-
-console.log(laterPart.toString());
-=======
 console.log(provided.replace('H', "'").substring(0,5));
 
 
@@ -53,4 +42,3 @@ var elevenEleven = (provided.length - 1)* 101;
 console.log(elevenEleven.toString());
 
 console.log(provided.split(' '));
->>>>>>> fac1126b575d7228d4364da6d400f48f7d4c7a8b
